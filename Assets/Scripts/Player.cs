b17bd260f5482ca4b191a97e5495f1ae30d9626d
Nathan Desages <nathan.desages@gmail.com>
2018-12-08 21:22:21 +0100

    added player movements

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public bool player1;
+	public float speed;
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x, z;
+		if (player1)
+		{
+			x = Input.GetAxis("HorizontalP1");
+			z = Input.GetAxis("VerticalP1");
+		}
+		else
+		{
+			x = Input.GetAxis("HorizontalP2");
+			z = Input.GetAxis("VerticalP2");
+		}
+
+		Vector3 velocity = new Vector3(x, 0, z) * speed * Time.deltaTime;
+
+		rb.MovePosition(transform.position + velocity);
+	}
+}
